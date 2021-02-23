2a620ba024f556d0cf5b0d08249db8ca2045a764
digeoesp <diegocrypto@outlook.com>
2021-02-23 13:43:25 -0800

    got array to work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Post from './Post'
 
 function App() {
-  let comments=['first', 'great post', 'hire this author']
+  
   return (
     <div >
-      <Post title="Dinosaurs are awesome" author="Stealthy Stegosaurus" body="Check out this body property!" comments="comment[0]"/>
-     <h1>hello</h1>
+      <Post title="Dinosaurs are awesome" author="Stealthy Stegosaurus" body="Check out this body property!" comments= {['First!', 'Great post', 'Hire this author now!']}/>
+     
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -4,7 +4,7 @@ function Post(props) {
             <h1> {props.title}</h1>
             <h1>By: {props.author}</h1>
             <h1>{props.body}</h1>
-            <h1>comments: {props.comments}</h1>
+            <h1>comments: {props.comments[1]}</h1>
         </div>
     )
 }
